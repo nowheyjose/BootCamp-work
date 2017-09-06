995720f2067b37343df51b37cbbcf7b7cf0638bf
Jose-Rafael <jdiaz5511@gmail.com>
2017-09-06 04:37:41 -0400

    completed assignment

--- listingController.js
+++ listingController.js
@@ -1,14 +1,29 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
+    $scope.query = {}
+    $scope.queryBy = '$'
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function(listings) {
+      $scope.listings.push({
+        'code': listings.code,
+        'name': listings.name,
+        'latitude': listings.coordinates.latitude,
+        'longitude': listings.coordinates.longitude,
+        'address': listings.address,
+      });
+      $scope.BD = {};
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.listings = index;
+    };
   }
 ]);
